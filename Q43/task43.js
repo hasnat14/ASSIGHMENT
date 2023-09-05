f2a29bddf7c72cdc5eac052ac8ce9d958de541d9
hasnat14 <143587903+hasnat14@users.noreply.github.com>
2023-09-05 14:58:16 +0500

    Add files via upload

---
+++ Q43/task43.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//let famous_magician=["Magic Man Fred.","Amazing Ace.","Mystic Max.","Illusionist Ivan.",]
+var Q42_1 = require("../Q42");
+var a = Q42_1.make_great;
+console.log(a);
+var Q41_1 = require("../Q41");
+Q41_1.show_magician;
---
+++ Q43/task43.ts
@@ -0,0 +1,8 @@
+//let famous_magician=["Magic Man Fred.","Amazing Ace.","Mystic Max.","Illusionist Ivan.",]
+import{ make_great } from '../Q42'
+let a=make_great
+console.log(a);
+import{ show_magician }from'../Q41'
+show_magician
+
+ 
